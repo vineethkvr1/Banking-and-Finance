5b168f9081aadabe576c2aa1460595f427b62b09
vineethkvr1 <140827678+vineethkvr1@users.noreply.github.com>
2023-10-28 17:02:39 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,16 @@
+#Initialize Terraform
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS provider
 provider "aws" {
-  region     = "ap-south-1"
-  
+  region = "ap-south-1"
 }
 
 # Create VPC
